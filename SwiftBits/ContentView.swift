a7cdd1ba5c1b0255a9e047d73da6400e51456e13
zhaoxiangyu <liseami@qq.com>
2025-08-22 16:39:14 +0800

    Initial Commit

---
+++ SwiftBits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftBits
+//
+//  Created by 赵翔宇 on 2025/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftBits/SwiftBitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBitsApp.swift
+//  SwiftBits
+//
+//  Created by 赵翔宇 on 2025/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
